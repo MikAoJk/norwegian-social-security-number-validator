97f444db6aaec2719f97ca80d85df679a00f8f5b
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2023-02-06 15:51:31 +0100

    Set jdk 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,9 @@ repositories {
 }
 
 java {
+    sourceCompatibility = JavaVersion.toVersion(javaVersion)
+    targetCompatibility = JavaVersion.toVersion(javaVersion)
+
     withJavadocJar()
     withSourcesJar()
 }
