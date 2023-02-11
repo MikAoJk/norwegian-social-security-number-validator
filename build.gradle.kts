0ecf172adb25af221e10133fae8977260298b8b4
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2023-02-11 02:26:33 +0100

    Release 1.0.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "io.github.MikAoJk"
-version = "1.0.7"
+version = "1.0.8"
 
 val junitJupiterVersion = "5.9.2"
 val kotlinVersion = "1.8.10"
