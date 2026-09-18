aa8a539fe41a68aa86943c9b5abd8b01e045ad01
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2026-09-18 20:29:37 +0200

    chorr: update ktfmt version to 0.56

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ group = "io.github.mikaojk"
 version = System.getenv("NEW_VERSION") ?: "1.0.0"
 
 val junitJupiterVersion = "6.1.3"
-val ktfmtVersion = "0.44"
+val ktfmtVersion = "0.56"
 
 plugins {
     kotlin("jvm") version "2.4.10"
